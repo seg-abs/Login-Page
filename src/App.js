5067b6cd9e0ef30fab051ac07cc1bed606cf5a28
seg <g.sathiyendran@gmail.com>
2023-01-02 12:13:34 +0530

    initiazied

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import { React } from 'react';
 import './App.scss';
-import Heading from './components/Heading';
-import Login from './components/Login';
-import Username from './components/Username';
-
 const App = () =>
 	<div className="App">
-		<Heading/>
-		<Login/>
-		<Username/>
+		<div>Header</div>
+		<div>Body</div>
 	</div>;
 
 export default App;
