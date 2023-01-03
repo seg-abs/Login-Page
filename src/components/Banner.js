e29813fdfb03b57b664ae28b4c2e2e7559884547
seg <g.sathiyendran@gmail.com>
2023-01-03 17:01:17 +0530

    simple components

---
+++ src/components/Banner.js
@@ -0,0 +1,9 @@
+import { React } from 'react';
+
+const Banner = () =>
+	<div className="banner">
+		<div className="rectangle1"/>
+		<div className="rectangle2"/>
+	</div>;
+
+export default Banner;
---
+++ src/components/CheckBox.js
@@ -0,0 +1,7 @@
+import { React } from 'react';
+
+const CheckBox = () =>
+	<div><input className="margin" type="CheckBox"/>
+		<span>Remember me</span></div>;
+
+export default CheckBox;
