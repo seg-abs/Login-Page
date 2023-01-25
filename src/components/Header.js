be94315543c1c3c61cbf25eea9010ca07cb6d97b
seg <g.sathiyendran@gmail.com>
2023-01-25 17:40:31 +0530

    refacted header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,16 @@ import homeIcon from '../Images/homeIcon.png';
 
 const Header = () =>
 	<div className="header">
-		<div>
+		<div className="homeIcon">
 			<img
 				className="home"
 				src={ homeIcon }
 				alt="home icon"
-			/></div>
+			/>
+		</div>
 		<div className="text">
-			<a href="http://www.google.com">
-				<span className="color"> Login/Register</span>
+			<a className="color" href="http://www.google.com">
+				Login/Register
 			</a></div>
 	</div>;
 
